1610cacbdd660928af9f5633ea95890d45b1ee0c
ozcanncnar <163649061+ozcanncnar@users.noreply.github.com>
2024-09-05 12:39:28 +0300

    Bonus sahnesi eklendi

--- Unity Essentials Project/Assets/_Unity Essentials/Scripts/Collectible.cs
+++ Unity Essentials Project/Assets/_Unity Essentials/Scripts/Collectible.cs
@@ -7,30 +7,38 @@ public class Collectible : MonoBehaviour
 
     public float rotationSpeed;
     public GameObject onCollectEffect;
+    private AudioSource audioSoruce;
     // Start is called before the first frame update
     void Start()
     {
+       audioSoruce = GetComponent<AudioSource>();
         
+
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Rotate(0, rotationSpeed, 0);
+        
     }
 
+    
+    
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            audioSoruce.Play();
             //Destroy the collectible
-            Destroy(gameObject);
+            Destroy(gameObject,audioSoruce.clip.length);
             //Instantiate the particle effect
             Instantiate(onCollectEffect, transform.position, transform.rotation);
 
-
+       
+            
         }
-
         
+
     }
 }
